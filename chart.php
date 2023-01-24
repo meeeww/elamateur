36c969874b3f6a1b37e0fa2671410f31b0182742
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-01-24 18:09:43 +0100

    zas

--- chart.php
+++ chart.php
@@ -1,59 +1,57 @@
 <?php
-include_once("db.php");
 
+include "db.php";
 
 ?>
 
 <html>
 
 <head>
-    <title>Generate Line Chart in PHP</title>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <style>
-        body {
-            background: #ccc;
-
-        }
-
-        #my-chart {
-            background: #fff;
-            padding: 20px;
-        }
-    </style>
-</head>
-
-<body>
-
-    <div id="my-chart" style="width: 100%; height: 400px;"></div>
     <script type="text/javascript">
         google.charts.load('current', {
-            'packages': ['corechart'],
-            'mapsApiKey': '' // her eyou can put you google map key
+            'packages': ['corechart']
         });
-        google.charts.setOnLoadCallback(drawRegionsMap);
+        google.charts.setOnLoadCallback(drawChart);
 
-        function drawRegionsMap() {
-            var data = google.visualization.arrayToDataTable([
-                ['Year', 'Sales', 'Expenses'],
+        function drawChart() {
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Fecha');
+            data.addColumn('number', 'LPS');
+            data.addRows([
                 <?php
-                $chartQuery = "SELECT * FROM historialRangos";
-                $chartQueryRecords = mysqli_query($con, $chartQuery);
-                while ($row = mysqli_fetch_assoc($chartQueryRecords)) {
-                    echo "['" . $row['fecha'] . "'," . $row['lps'] . "," . $row['lps'] . "],";
+                $rango;
+                $query = "SELECT * FROM historialRangos WHERE idJugador = 5";
+                $res = mysqli_query($conn, $query);
+                $result = mysqli_num_rows($res);
+                if ($result > 0) {
+                    while ($row = mysqli_fetch_assoc($res)) {
+                        echo "['" . $row["fecha"] . "', " . $row["lps"] . "],";
+                        //$rango = $row["division"] + $row["rango"];
+                        $rango = substr($row["division"], 0);
+                    }
                 }
                 ?>
             ]);
 
             var options = {
-
+                curveType: 'function',
+                legend: {
+                    position: 'none'
+                }
             };
 
-            var chart = new google.visualization.LineChart(document.getElementById('my-chart'));
+            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
             chart.draw(data, options);
         }
     </script>
-    </head>
+</head>
+
+<body>
+    <div class="graficoChart">
+        <h3 class="hola">Hola</h3>
+        <div id="curve_chart" style="width:900px; height:500px"></div>
+    </div>
 </body>
 
 </html>
