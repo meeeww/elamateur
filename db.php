559e8df926038019f1963b7bd4ec7316fd687a78
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-02-08 16:53:56 +0100

    zas

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 
-    $usuario = "discordbot";
-    $pass = "vMGhh64@tr";
+    $usuario = "web";
+    $pass = "W~5775kqi";
     $host = "212.227.32.40";
     $dbname = "elamateur";
 
     $conn = mysqli_connect($host, $usuario, $pass, $dbname);
 
 
-
+//W~5775kqi
 ?>
