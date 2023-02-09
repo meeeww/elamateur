8024cc612eb79d63d911bc08af531e12bddc8228
mew <102923191+meeeww@users.noreply.github.com>
2023-02-09 14:00:44 +0100

    zas

--- db.php
+++ db.php
@@ -5,6 +5,11 @@
     $host = "212.227.32.40";
     $dbname = "elamateur";
 
+    //$usuario = "dbu5590175";
+    //$pass = "@5qKo2H@O0Nfy&858ZK9";
+    //$host = "db5011880292.hosting-data.io";
+    //$dbname = "dbs10005204";
+
     $conn = mysqli_connect($host, $usuario, $pass, $dbname);
 
 
